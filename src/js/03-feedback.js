d101dfc0b3c1b8516bd029558ec79ada2f29dccc
emmabaka <bakaemma88@gmail.com>
2023-02-23 19:47:07 +0200

    task 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+var throttle = require('lodash.throttle');
 
+const formEl = document.querySelector('.feedback-form');
+const emailEl = document.querySelector('input[type="email"]');
+const messageEl = document.querySelector('textarea[name="message"]');
+
+function setInfo() {
+  if (localStorage.getItem('feedback-form-state') === null) {
+    emailEl.value = '';
+    messageEl.value = '';
+  } else {
+    emailEl.value = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    ).email;
+    messageEl.value = JSON.parse(
+      localStorage.getItem('feedback-form-state')
+    ).message;
+  }
+}
+setInfo();
+
+formEl.addEventListener(
+  'input',
+  throttle(() => {
+    localStorage.setItem(
+      'feedback-form-state',
+      JSON.stringify({ email: emailEl.value, message: messageEl.value })
+    );
+  }, 500)
+);
+
+formEl.addEventListener('submit', e => {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem('feedback-form-state')));
+  localStorage.removeItem('feedback-form-state');
+  emailEl.value = '';
+  messageEl.value = '';
+});
